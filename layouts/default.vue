abf69b76ad7dc8cfac71a0317d34231f6a6e0688
Hosoo <hosoo.xoc@gmail.com>
2020-12-06 22:23:32 +0800

    2020 update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,5 @@
 <template>
-  <div>
-    <div class="area" >
-      <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </div >
-    <transition name="fade-in">
-      <nuxt/>
-    </transition>
-  </div>
+	<div class="container">
+		<nuxt />
+	</div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,54 @@
 <template>
-  <div class="index-container">
-    <div class="link-wrapper">
-      <nuxt-link class="button link" to="about">ABOUT</nuxt-link>
-      <nuxt-link class="button link" to="contact">CONTACT</nuxt-link>
-    </div>
-    <div class="text-wrapper">
-      <h2 class="greetings">Hey</h2>
-      <h1 class="name">I'm Khosoo</h1>
-    </div>
-    <div class="personal-links">
-      <a target="_blank" href="https://instagram.com/hosukechi" class="link">Instagram</a>
-      <a target="_blank" href="#" class="link">Github</a>
-      <a target="_blank" href="https://gitlab.com/nothosoo" class="link">Gitlab</a>
-    </div>
-  </div>
+	<div class="index-container">
+		<div class="text-wrapper">
+			<h2 class="greetings">Hello</h2>
+			<h1 class="name">My name is Khosbayar</h1>
+			<p>
+				I am a software developer based in Ulaanbaatar, Mongolia. Most of my
+				works are related to web and mobile. You can find me on
+				<a target="_blank" href="https://instagram.com/hosoothe" class="link"
+					>Instagram</a
+				>
+				and
+				<a
+					target="_blank"
+					href="https://www.linkedin.com/in/khosbayar-molomjamts/"
+					class="link"
+					>LinkedIn</a
+				>
+			</p>
+			<p>
+				Some of the works that I have done: <br />
+				<a target="_blank" href="https://vyred.chat" class="link">vyred.chat</a
+				>, <a target="_blank" href="https://mfe.mn" class="link">mfe.mn</a>,
+				<a target="_blank" href="https://capitis.mn" class="link">capitis.mn</a
+				>,
+				<a target="_blank" href="https://katanasub.me" class="link"
+					>katanasub.me</a
+				>,
+				<a
+					target="_blank"
+					href="https://play.google.com/store/apps/details?id=com.mfe.mn.mfe"
+					class="link"
+					>mfe</a
+				>
+			</p>
+			<p>
+				Projects I have contributed: <br />
+				<a target="_blank" href="https://insurevite.com/" class="link"
+					>insurevite.com</a
+				>
+			</p>
+		</div>
+	</div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    
-  }
+	components: {},
 }
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
