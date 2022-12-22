032f59be2e8fd6a296f97b830e2e5f5bd1344d88
nothosoo <hosoo.xoc@gmail.com>
2022-12-22 19:13:12 +0800

    update: 2022

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 			<h2 class="greetings">Hello</h2>
 			<h1 class="name">My name is Khosbayar</h1>
 			<p>
-				I am a software developer based in Ulaanbaatar, Mongolia. Most of my
+				I am a software engineer based in Ulaanbaatar, Mongolia. Most of my
 				works are related to web and mobile. You can find me on
 				<a target="_blank" href="https://instagram.com/hosoothe" class="link"
 					>Instagram</a
@@ -19,12 +19,10 @@
 			</p>
 			<p>
 				Some of the works that I have done: <br />
+				<a target="_blank" href="https://worki.mn/" class="link">worki.mn</a>,
 				<a target="_blank" href="https://vyred.chat" class="link">vyred.chat</a
 				>, <a target="_blank" href="https://mfe.mn" class="link">mfe.mn</a>,
 				<a target="_blank" href="https://capitis.mn" class="link">capitis.mn</a
-				>,
-				<a target="_blank" href="https://katanasub.me" class="link"
-					>katanasub.me</a
 				>,
 				<a
 					target="_blank"
@@ -37,6 +35,9 @@
 				Projects I have contributed: <br />
 				<a target="_blank" href="https://insurevite.com/" class="link"
 					>insurevite.com</a
+				>,
+				<a target="_blank" href="https://www.corex.mn/" class="link"
+					>corex.mn</a
 				>
 			</p>
 		</div>
@@ -44,11 +45,9 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
 	components: {},
-}
+};
 </script>
 
 <style lang="scss"></style>
